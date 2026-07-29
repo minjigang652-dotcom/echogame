a1eacb5c9dcf615dc78916dfdce892b276b36c63
minjigang652-dotcom <minjigang652@gmail.com>
2026-07-29 20:30:25 +0900

    Update VideoSchool.jsx

--- src/VideoSchool.jsx
+++ src/VideoSchool.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { C, NetContext, PixelHouse, Hero, Panel, PxButton, TitleBar } from "./LittleJuniorWorld.jsx";
 
 /* ======================= 스쿨(네이버/영상) ======================= */
